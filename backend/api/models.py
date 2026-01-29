959ae97d2aa5e108b160ca452b7dbfe25498cde9
mama0048 <mama0048@uni.sydney.edu.au>
2026-01-29 12:57:52 +1100

    backend: setup model Event and User

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class Event(models.Model):
+    """
+    create defaults in a bit
+    """
+    id = models.UUIDField()
+    title = models.CharField() # add max
+    pinned = models.BooleanField()
+    dateCreated = models.DateTimeField() # set default timezone
+    startDate = models.DateField()
+    endDate = models.DateField()
+    status = models.BooleanField()
+    bestAvailability = models.DateTimeField()
+    participants = ArrayField(models.CharField)
+    numParticipants = models.IntegerField()
+    numResponded = models.IntegerField()
+    numAvailable = models.IntegerField()
+    tag = models.CharField()
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
+    
+class User(models.Model):
+    id = models.UUIDField()
+    username = models.CharField()
+    displayName = models.CharField()
+    timezone = models.CharField()
+    events = ArrayField(models.ForeignKey(Event))
+
+    def __str__(self):
+        return self.username
