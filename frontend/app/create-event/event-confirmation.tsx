a3a6dc5a20ebe41027d64d40589cc3ef7b0db94d
--global <declan.rsg@gmail.com>
2026-02-04 00:17:00 +1100

    refactor: separate formatting helpers

--- frontend/app/create-event/event-confirmation.tsx
+++ frontend/app/create-event/event-confirmation.tsx
@@ -1,42 +1,12 @@
 import { View, Text, Pressable, StyleSheet } from "react-native"
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { confirmationStyles, generalStyles } from './styles';
+import {formatDate, formateDateTime, formatTime} from "../../utils/formatting-helpers";
 
 export default function EventConfirmation() {
     const router = useRouter();
     const params = useLocalSearchParams();
 
-    function formatDate(str: string | string[]) {
-        if(Array.isArray(str)) {
-            str = str[0];
-        }
-        return new Date(str).toLocaleDateString("en-GB",{
-            day: "2-digit",
-            month: "short",
-            year: "numeric"
-        });
-    }
-
-    function formatTime(str: string | string[]) {
-        if(Array.isArray(str)) {
-            str = str[0];
-        }
-        const split = str.split(":");
-        const hr = (parseInt(split[0]) % 12) || 12;
-        const ampm = parseInt(split[0]) >= 12 ? "pm" : "am";
-        return hr + ":" + split[1] + " " + ampm;
-    }
-
-    function formateDateTime(str: string | string[]) {
-        if(Array.isArray(str)) {
-            str = str[0];
-        }
-        const split = str.split("T");
-        const date = formatDate(split[0]);
-        const time = formatTime(split[1]);
-
-        return date + ", " + time;
-    }
 
     return (
         <View style={generalStyles.container}>
